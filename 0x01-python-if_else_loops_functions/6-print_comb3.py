def03ea67278b9471d5ca52745d4e365c42267e8
ProgressAce <daiyaansallie7@gmail.com>
2023-03-07 12:23:30 -0800

    string format fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for x in range(10):
-    for y in range(10):
+    for y in range(x + 1, 10):
         if x == 8 and y == 9:
-            print('{:d}{:d}'.format(x, y))
+            print('89')
         elif y > x:
             print('{:d}{:d}, '.format(x, y), end='')
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,8 @@ def uppercase(str1):
     for letter in str1:
         code = ord(letter)
         if code >= 97 and code <= 122:
-            print('{:c}'.format(code - 32), end='')
-        else:
-            print('{}'.format(letter), end='')
+            code -= 32
+
+        print('{:c}'.format(code), end='')
+
     print('')
