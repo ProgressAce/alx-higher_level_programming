b7b7bc97975cf5e3624a01099fc6cc2daf8cde73
ProgressAce <daiyaansallie7@gmail.com>
2023-03-28 09:09:46 -0700

    Add solution for task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Defines a square class with an attribute"""
+
+class Square:
+    """Describes a square"""
+
+    def __init__(self, size=0):
+        """Initialises the square
+        Args:
+            size(int): the size of the square
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size < 0
+        """
+
+        if  isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
