88c157642de62782787ee9defbaae64f244cedad
ProgressAce <daiyaansallie7@gmail.com>
2023-03-28 09:09:30 -0700

    Add solution for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a square class with an attribute"""
+
+class Square:
+    """Describes a square"""
+
+    def __init__(self, size):
+        """Initialises the square
+        Args:
+            size(int): the size of the square
+        """
+
+        self.__size = size
+
