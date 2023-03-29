32501ec2305880c5080caa5d61f7397e80164d34
ProgressAce <daiyaansallie7@gmail.com>
2023-03-29 09:14:43 -0700

    Update solution

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            print(my_list[i], end="")
+    """Prints x elements of a list, printed on the same line"""
+    count = 0
+
+    for i in range(x):
+        try:
+            print(f'{my_list[i]}', end="")
             count += 1
+        except IndexError:
+            break
 
-        print()
+    print()
 
-    except IndexError:
-        print()
-
-    return count
+    return (count)
