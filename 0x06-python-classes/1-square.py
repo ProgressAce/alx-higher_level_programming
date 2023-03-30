bdd1ded41aeb9e41b1a9c438cf0f1fc6d0000fca
ProgressAce <daiyaansallie7@gmail.com>
2023-03-29 22:54:00 -0700

    Comply to pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 """Defines a square class with an attribute"""
 
+
 class Square:
-    """Describes a square"""
+    """Describes a square object"""
 
     def __init__(self, size):
         """Initialises the square
@@ -12,4 +12,3 @@ class Square:
         """
 
         self.__size = size
-
