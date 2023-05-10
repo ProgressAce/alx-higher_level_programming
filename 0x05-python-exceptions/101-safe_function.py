0cf97efef0fab35c31aa1feee781efe49309dabd
ProgressAce <daiyaansallie7@gmail.com>
2023-05-10 07:41:53 -0700

    Change syntax

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -8,6 +8,7 @@ def safe_function(fct, *args):
 
     try:
         result = fct(*args)
-        return (result)
-    except Exception:
-        print('Exception: {}'.format(sys.exc_info()[1], file=sys.stderr))
+        return result
+    except (IndexError, ValueError, TypeError, ZeroDivisionError) as err:
+        print('Exception: {}'.format(err), file=sys.stderr)
+        return None
