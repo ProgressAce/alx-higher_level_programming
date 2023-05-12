5b673cc1b65288b06b933459a38441d2ad62045e
ProgressAce <daiyaansallie7@gmail.com>
2023-05-12 02:19:12 -0700

    add string format specifier

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num == 99:
-        print('{}'.format(num))
+        print('{:d}'.format(num))
     else:
         print('{:02d}, '.format(num), end='')  # print width of 2 spaces
