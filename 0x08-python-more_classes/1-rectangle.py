675e826988487c003d980847afa932f64fc42704
SubtleDaiyaan <daiyaansallie7@gmail.com>
2023-06-07 05:41:08 -0700

    change order of private attributes defition

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
     """Blueprints for a Rectangle instance."""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
