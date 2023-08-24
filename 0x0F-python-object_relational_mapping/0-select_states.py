fd437a9e7bdac7afe6b06a7a321366c3fe02b313
ProgressAce <daiyaans077@gmail.com>
2023-08-24 08:54:36 -0700

    change: hostname of database connection

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ import MySQLdb
 def main():
     """The entry execution of the script."""
 
-    db = MySQLdb.connect(host="127.0.0.1", port=3306,
+    db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
