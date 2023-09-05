f946e4b6ab28d5c8b17ac65114ac357f0739e91e
ProgressAce <daiyaansallie7@gmail.com>
2023-09-05 12:49:36 -0700

    add: task 5-10 solutions

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str += c;
+      }
+      console.log(str);
+    }
+
+  }
+}
+
+module.exports = Square;
