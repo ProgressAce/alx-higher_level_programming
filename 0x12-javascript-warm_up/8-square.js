342290930caf72edc1d5c8917523f717afb50324
ProgressAce <daiyaansallie7@gmail.com>
2023-09-11 11:18:46 -0700

    style: semistandard compliant

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    sq = '';
+    let sq = '';
 
     for (let j = 0; j < size; j++) {
       sq += 'X';
