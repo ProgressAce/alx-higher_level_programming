9325170533f9f68c0a01feedda531b97ba4f6693
ProgressAce <daiyaansallie7@gmail.com>
2023-09-12 04:59:01 -0700

    add: scripts 0-1

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Script that creates an empty class defining a Rectangle.
+
+class Rectangle {
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Creates a class that represents a Rectangle.
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
