65bfffa5ddec000f95a6fc4cbca7edadad8648b2
ProgressAce <daiyaansallie7@gmail.com>
2023-09-12 12:22:42 -0700

    bug: fix list index out of range

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,10 +2,10 @@
 // Creates a function that returns the reversed version of a list.
 
 exports.esrever = function (list) {
-  let length = list.length;
+  let length = list.length - 1;
 
   for (let i = 0; i < list.length / 2; i++) {
-    let temp = list[i];
+    const temp = list[i];
     list[i] = list[length];
     list[length] = temp;
 
