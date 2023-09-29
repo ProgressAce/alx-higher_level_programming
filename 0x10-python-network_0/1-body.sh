d6b9f2fdad0ce0180da18cc31c4bb8a3bf1890cd
Daiyaan <96121165+ProgressAce@users.noreply.github.com>
2023-09-29 06:21:53 +0200

    remove: directory 0x10-python-network_0 for redo

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends a GET request to the URL, and displays the body of the HTTP response
-curl -sL "$1"
--- 0x10-python-network_0/3-methods.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# displays all HTTP methods the URL server will accept.
-curl -sI "$1" | grep Allow | cut -d " " -f 2-4
